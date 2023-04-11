cf6cd49c8efb0ff9b5956010ff7a5cb5e4e8e9fa
willwoodward <will@woodwardweb.com>
2023-04-12 00:25:52 +0100

    Added fontawesome icons and added collapsing navbar

--- website/src/components/sideNavbar.vue
+++ website/src/components/sideNavbar.vue
@@ -8,17 +8,22 @@
 </script>
 
 <template>
-    <div class="bg-zinc-900 py-6 transition-all" :class="navbarOpen ? 'w-32': 'w-8'" id="navbar">
+    <div class="bg-zinc-900 py-6 transition-all" :class="navbarOpen ? 'w-32': 'w-12'" id="navbar">
         <div class="flex flex-row">
-          <div class="basis-3/4">1</div>
-          <div @click="closeNavbar" class="basis-1/4 hover:text-white">X</div>
+          <div v-show="navbarOpen" class="basis-3/4"></div>
+          <div @click="closeNavbar" class="basis-1/4 hover:text-white" :class="{'mx-4': !navbarOpen }">
+            <font-awesome-icon :icon="navbarOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-arrow-right'" style="color: #ffffff; width: 100%;"/>
+          </div>
         </div>
         <!-- X to close -->
-        <ul v-for="item in listItems" :key="item.id">
+        <div v-for="item in listItems" :key="item.id">
           <RouterLink :to="item.link">
-            <li class="p-4 hover:text-white hover:border-r-4 hover:bg-zinc-800 transition-all font-sans" :class="{'text-white': item.name === this.selected}">{{ item.name }}</li>
+            <div class="p-4 hover:text-white hover:border-r-4 hover:bg-zinc-800 transition-all font-sans" :class="{'text-white': item.name === this.selected}">
+              <font-awesome-icon :icon="item.icon" style="color: #ffffff;" />
+              {{ navbarOpen ? item.name : null }}
+            </div>
           </RouterLink>        
-        </ul>
+        </div>
     </div>
 </template>
 
@@ -27,9 +32,9 @@
     data: function () {
       return {
         listItems: [
-          {id: 0, name: 'Home', link: "/"},
-          {id: 1, name: 'Members', link: "/members"},
-          {id: 2, name: 'Clan Wars', link: "/clanwars"}
+          {id: 0, name: 'Home', link: "/", icon: 'fa-solid fa-house'},
+          {id: 1, name: 'Members', link: "/members", icon: 'fa-solid fa-users'},
+          {id: 2, name: 'Clan Wars', link: "/clanwars", icon: 'fa-solid fa-fire'}
         ],
         navbarOpen: true
       }
--- website/src/main.js
+++ website/src/main.js
@@ -2,10 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
-
 import './assets/main.css'
 
-const app = createApp(App)
+// Font Awesome Icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faFire } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faXmark, faHouse, faUsers, faFire, faArrowRight)
+
+const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
