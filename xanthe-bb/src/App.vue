1b31857c782a9092f20816342ccfb0d098c70874
Jacob Wrenn <jacob@wrenn.me>
2023-04-16 12:52:26 +0100

    refactor to allow for select dropdown

--- xanthe-bb/src/App.vue
+++ xanthe-bb/src/App.vue
@@ -1,17 +1,31 @@
 <script setup>
-import Latest from './components/Latest.vue'
+import { useFirestore, useCollection } from 'vuefire'
+import { collection, query, orderBy } from 'firebase/firestore'
+import { ref, watch } from 'vue'
+import Table from './components/Table.vue'
+
+const db = useFirestore()
+
+let currentRaid = ref('')
+
+const raids = useCollection(query(collection(db, 'raids'), orderBy('date', 'desc')))
+
+watch(raids, (newRaids) => {
+  if (newRaids.length > 0 && currentRaid.value == '') {
+    currentRaid = newRaids[0].id
+  }
+})
 </script>
 
 <template>
-  <Suspense>
-    <!-- component with nested async dependencies -->
-    <Latest />
-
-    <!-- loading state via #fallback slot -->
-    <template #fallback>
-      <div class="flex justify-center p-16 text-2xl">
-        Loading...
-      </div>
-    </template>
-  </Suspense>
+  <div class="p-16">
+    <h1 class="text-4xl">Xanthe BB Raid Weekends</h1>
+    <div class="flex mt-8">
+      <p>Viewing data from weekend ending:</p>
+      <select class="ml-8" v-model="currentRaid">
+        <option v-for="raid in raids" :key="raid.id" :value="raid.id">{{ raid.date.toDate().toLocaleDateString('en-GB') }}</option>
+      </select>
+    </div>
+    <Table v-if="currentRaid" :id="currentRaid"></Table>
+  </div>
 </template>
